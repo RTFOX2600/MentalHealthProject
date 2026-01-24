bbb00dd8aaa674b516cfa8095ca228a9ebdd3659
RTFox <2652205627@qq.com>
2026-01-24 14:02:04 +0800

    添加关于页面

--- school_platform/urls.py
+++ school_platform/urls.py
@@ -21,5 +21,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
